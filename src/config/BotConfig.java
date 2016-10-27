900ae4a4a0f84dc4f8621ddd8ebc4cf6e7fbdfba
Clevero <Clevero@users.noreply.github.com>
2016-10-27 21:04:51 +0200

    Update BotConfig.java

--- src/config/BotConfig.java
+++ src/config/BotConfig.java
@@ -3,9 +3,9 @@ package config;
 public interface BotConfig {
 
 //	public final String ON_START = "";
-//	public final String BOT_KEY = "223021247:AAGIJrQwbxF0jMMygUIjyd50OqBM5ei216c";
+//	public final String BOT_KEY = "<token>";
 //	public final String BOT_USERNAME = "Expertsupportbot";
-//	public final Long ADMIN_ID = new Long(203887950);
+//	public final Long ADMIN_ID = new Long(<admin_id>);
 //	public final String ON_BLOCK = "Violation is not allowed. You are now blocked.";
 //	public final String BLOCKED = "You are blocked. You cannot contact the admin through me.";
 //	public final String ON_UNBLOCKED = "You are now unblocked.";
